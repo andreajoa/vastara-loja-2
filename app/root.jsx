652e78f898866755b2756429bcaf69b1a529b977
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-04-03 00:23:09 -0300

    feat: add Google Tag Manager GTM-TRLDBT7Q

--- app/root.jsx
+++ app/root.jsx
@@ -57,6 +57,9 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+      {/* Google Tag Manager */}
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TRLDBT7Q');`}} />
+      {/* End Google Tag Manager */}
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="theme-color" content="#0a0a0a" />
@@ -102,6 +105,9 @@ export default function App() {
         })}} />
       </head>
       <body>
+      {/* Google Tag Manager (noscript) */}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRLDBT7Q" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+      {/* End Google Tag Manager (noscript) */}
         <Analytics.Provider cart={data.cart} shop={data.shop} consent={data.consent}>
           <GoogleAnalytics measurementId='G-LW70Z8LP18' />
           <Layout header={data.header} footer={data.footer}>
